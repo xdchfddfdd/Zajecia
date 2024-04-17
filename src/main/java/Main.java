82c9a383b2f687c1e7a4b29cca19ffd0f03668e8
xdchfddfdd <161622667+xdchfddfdd@users.noreply.github.com>
2024-04-17 14:53:50 +0000

    3_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,29 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
-import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        char[][] tablica = generujTablice(6, 6);
-        wyswietlTablice(tablica);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj wysokość trójkąta:");
+        int height = scanner.nextInt();
+
+        System.out.println("Podaj znak, z którego ma być zbudowany trójkąt:");
+        char symbol = scanner.next().charAt(0);
+
+        generateTriangle(height, symbol);
     }
 
-    public static char[][] generujTablice(int wiersze, int kolumny) {
-        char[][] tablica = new char[wiersze][kolumny];
-        Random rand = new Random();
-
-        for (int i = 0; i < wiersze; i++) {
-            for (int j = 0; j < kolumny; j++) {
-                // Generowanie losowego znaku ASCII
-                char znak = (char) (rand.nextInt(94) + 33); // Zakres od 33 do 126 (włącznie) dla drukowalnych znaków ASCII
-                tablica[i][j] = znak;
-            }
-        }
-
-        return tablica;
-    }
-
-    public static void wyswietlTablice(char[][] tablica) {
-        for (int i = 0; i < tablica.length; i++) {
-            for (int j = 0; j < tablica[i].length; j++) {
-                System.out.print(tablica[i][j] + " ");
+    public static void generateTriangle(int height, char symbol) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(symbol + " ");
             }
             System.out.println();
         }
     }
-  
 }
 
 
